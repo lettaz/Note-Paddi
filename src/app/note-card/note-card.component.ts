5b592d714aa20de6152fba645142296c0315f8dc
lettaz <maconzy12@gmail.com>
2022-06-20 08:39:34 +0200

    UI implementation

---
+++ src/app/note-card/note-card.component.ts
@@ -0,0 +1,42 @@
+import { AfterViewInit, Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-note-card',
+  templateUrl: './note-card.component.html',
+  styleUrls: ['./note-card.component.scss']
+})
+export class NoteCardComponent implements AfterViewInit {
+
+  @Input() title: string = '';
+  @Input() body: string = '';
+
+  @ViewChild('truncator')
+  truncator!: ElementRef<HTMLElement> ;
+  @ViewChild('bodyText')
+  bodyText!: ElementRef<HTMLElement>;
+
+
+
+
+  constructor(private renderer:Renderer2) { 
+    
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  ngAfterViewInit(): void{
+//Work out if there is a text overflow and if not, then hide the trucator
+
+let style = window.getComputedStyle(this.bodyText.nativeElement, null);
+let viewableHeight = parseInt(style.getPropertyValue("height"), 10);
+
+if(this.bodyText.nativeElement.scrollHeight > viewableHeight){
+  this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block'); 
+}else{
+  this.renderer.setStyle(this.truncator.nativeElement, 'display', 'none'); 
+}
+}
+
+}
